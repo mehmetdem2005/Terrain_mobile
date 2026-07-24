56bf50dd29f33dd065e007493dcccbc7c5a6c6e2
mehmetdem2005 <mehmetdem2005@gmail.com>
2026-07-24 21:11:33 +0300

    make deformation backend dependency path-explicit

--- addons/island_terrain/deformation/null_deformation_backend.gd
+++ addons/island_terrain/deformation/null_deformation_backend.gd
@@ -1,5 +1,5 @@
 @tool
-extends IslandTerrainDeformationBackend
+extends "res://addons/island_terrain/deformation/terrain_deformation_backend.gd"
 class_name IslandTerrainNullDeformationBackend
 
 # Deliberate no-op backend used until the sparse voxel/SDF phase is enabled.
